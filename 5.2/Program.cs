21c209e55b887528ac6d253486771c0f225941ee
Mihael Kardash <mixan193@gmail.com>
2023-07-03 17:59:40 +0300

    Добавьте файлы проекта.

---
+++ 5.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReversWords(Console.ReadLine()));
+            Console.ReadKey();
+        }
+        private static string[] SplitText(string text)
+        {
+            return text.Split(' ');
+        }
+        private static string ReversWords(string inputPhrase)
+        {
+            string[] splitedText = SplitText(inputPhrase);
+            string result = "";
+            for(int i = splitedText.Length - 1; i >= 0; i--)
+            {
+                result += splitedText[i] + ' ';
+            }
+            return result;
+        }
+    }
+}
